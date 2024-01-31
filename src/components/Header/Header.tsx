36fddd614915b6068e56ab8af021176cf776c42d
Muneyy <muneyyyy@gmail.com>
2024-01-31 19:33:38 +0800

    feature/mobile-header-with-sidebar
    prettier fix- add eslint rules for unused imports and unused vars
    
    Add layout for header
    
    Layout for sidebar
    
    Add sidebar animation

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import hamburgerMenu from "@/assets/images/icon-menu.svg";
+import logoSvg from "@/assets/images/logo.svg";
+import cartSvg from "@/assets/images/icon-cart.svg";
+import avatarPng from "@/assets/images/image-avatar.png";
+import styles from "./Header.module.sass";
+import Image from "next/image";
+
+export default function Header({
+  toggleSidebar,
+}: {
+  toggleSidebar: () => void;
+}) {
+  return (
+    <header className={styles.header}>
+      <section className={styles.headerLeftSide}>
+        <button onClick={toggleSidebar} className={styles.menuButton}>
+          <Image src={hamburgerMenu} alt="hamburger menu" />
+        </button>
+      </section>
+      <section className={styles.headerRightSide}>
+        <div className="logo">
+          <Image
+            className={styles.companyLogo}
+            src={logoSvg}
+            alt="sneakers company logo"
+          />
+        </div>
+        <div className={styles.userAndAvatar}>
+          <button className={styles.cartButton}>
+            <Image src={cartSvg} alt="shopping cart icon" />
+          </button>
+          <Image
+            className={styles.avatar}
+            src={avatarPng}
+            alt="icon for user avatar, a long-haired man with sunglasses"
+            width={29}
+          />
+        </div>
+      </section>
+    </header>
+  );
+}
