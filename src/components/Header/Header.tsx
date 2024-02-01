077457c8c466f70cb6442a0a3370f1ff4bcf13d3
Muneyy <muneyyyy@gmail.com>
2024-02-01 18:17:50 +0800

    feature/mobile-cart
    Finish cart layout
    
    Add layout for cart items
    
    Add delete functionality
    
    Remove temporary quantity display

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import Image from "next/image";
-import { useContext, useRef, useState } from "react";
+import { useState } from "react";
 import avatarPng from "@/assets/images/image-avatar.png";
 import styles from "./Header.module.sass";
 import { CartSvg, LogoSvg, MenuSvg } from "@/assets/svgs/SvgComponents";
 import Cart from "./SubComponents/Cart";
-import { CartContext } from "@/context/CartContext";
 
 export default function Header({
   toggleSidebar,
@@ -14,13 +13,10 @@ export default function Header({
   toggleSidebar: () => void;
 }) {
   const [isCartShown, setIsCartShown] = useState<boolean>(false);
-  const { cart } = useContext(CartContext);
 
   const handleCartClick = () => {
     setIsCartShown((prevState) => !prevState);
   };
-
-  const buttonRef = useRef(null);
 
   return (
     <header className={styles.header}>
@@ -44,14 +40,8 @@ export default function Header({
             className={styles.cartButton}
             aria-label="Toggle shopping cart"
             onClick={handleCartClick}
-            ref={buttonRef}
           >
             <CartSvg />
-            {cart.length > 0 && (
-              <span className={styles.cartQuantityDisplay}>
-                {cart[0].quantity}
-              </span>
-            )}
           </button>
           <Image
             className={styles.avatar}
@@ -59,12 +49,7 @@ export default function Header({
             alt="icon for user avatar, a long-haired man with sunglasses"
             width={29}
           />
-          {isCartShown && (
-            <Cart
-              buttonRef={buttonRef}
-              setIsCartShown={setIsCartShown}
-            />
-          )}
+          {isCartShown && <Cart />}
         </div>
       </section>
     </header>
--- src/components/Header/SubComponents/Cart.tsx
+++ src/components/Header/SubComponents/Cart.tsx
@@ -1,42 +1,13 @@
-import React, { useContext, useEffect, useRef } from "react";
+import { useContext } from "react";
 import styles from "./Cart.module.sass";
 import { CartContext } from "@/context/CartContext";
 import CartItem from "./CartItem";
 
-export default function Cart({ buttonRef, setIsCartShown } : {
-  buttonRef: React.RefObject<HTMLButtonElement>;
-  setIsCartShown: React.Dispatch<React.SetStateAction<boolean>>;
-}) {
+export default function Cart() {
   const { cart } = useContext(CartContext);
 
-  const cartRef = useRef(null);
-
-  // https://stackoverflow.com/questions/32553158/detect-click-outside-react-component
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      const isOutsideClicked = cartRef.current
-        && !(cartRef.current as HTMLElement).contains(event.target as Node);
-      const isButtonClicked = buttonRef.current
-        && (buttonRef.current as HTMLButtonElement).contains(event.target as Node);
-      // if button is clicked (which is also outside of cart), do nothing
-      if (isOutsideClicked && isButtonClicked) {
-        return;
-      }
-      // if outside of cart is clicked, close cart
-      if (isOutsideClicked) {
-        setIsCartShown(false);
-      }
-    };
-    // Bind the event listener
-    document.addEventListener("mouseup", handleClickOutside);
-    return () => {
-      // Unbind the event listener on clean up
-      document.removeEventListener("mouseup", handleClickOutside);
-    };
-  }, [cartRef, buttonRef, setIsCartShown]);
-
   return (
-    <aside ref={cartRef} className={styles.cartCard}>
+    <aside className={styles.cartCard}>
       <header>
         <span>Cart</span>
       </header>
