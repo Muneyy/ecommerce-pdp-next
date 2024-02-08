4fcdc0d86d26fd1490b3df1721ab78348a59d805
Muneyy <muneyyyy@gmail.com>
2024-02-08 23:49:45 +0800

    Add close cart feature when clicked outside, add hover for add to cart button

--- src/components/Header/SubComponents/Cart.tsx
+++ src/components/Header/SubComponents/Cart.tsx
@@ -1,13 +1,42 @@
-import { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import styles from "./Cart.module.sass";
 import { CartContext } from "@/context/CartContext";
 import CartItem from "./CartItem";
 
-export default function Cart() {
+export default function Cart({ buttonRef, setIsCartShown } : {
+  buttonRef: React.RefObject<HTMLButtonElement>;
+  setIsCartShown: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   const { cart } = useContext(CartContext);
 
+  const cartRef = useRef(null);
+
+  // https://stackoverflow.com/questions/32553158/detect-click-outside-react-component
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const isOutsideClicked = cartRef.current
+        && !(cartRef.current as HTMLElement).contains(event.target as Node);
+      const isButtonClicked = buttonRef.current
+        && (buttonRef.current as HTMLButtonElement).contains(event.target as Node);
+      // if button is clicked (which is also outside of cart), do nothing
+      if (isOutsideClicked && isButtonClicked) {
+        return;
+      }
+      // if outside of cart is clicked, close cart
+      if (isOutsideClicked) {
+        setIsCartShown(false);
+      }
+    };
+    // Bind the event listener
+    document.addEventListener("mouseup", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mouseup", handleClickOutside);
+    };
+  }, [cartRef, buttonRef, setIsCartShown]);
+
   return (
-    <aside className={styles.cartCard}>
+    <aside ref={cartRef} className={styles.cartCard}>
       <header>
         <span>Cart</span>
       </header>
